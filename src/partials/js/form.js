41cfcc6698115da87aa88582e06c2428add41e5c
Valeria Sihrianska <132007746+ValeriaSihr@users.noreply.github.com>
2025-04-18 14:02:49 +0100

    form

--- src/partials/js/form.js
+++ src/partials/js/form.js
@@ -1,1 +1,43 @@
-console.log("Goodbye world");
+const STORAGE_KEY = "feedback-form-data";
+
+let formData = {
+  email: "",
+  message: "",
+}
+
+const form = document.querySelector(".feedback-form")
+const emailInput = document.querySelector("[name = 'email']");
+const messageInput = document.querySelector("[name = 'message']");
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData)
+  emailInput.value = formData.email || "";
+  messageInput.value = formData.message || "";
+
+}
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  if (name in formData) {
+    formData[name] = value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  };
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert("Plese, fill all fields!");
+    return;
+  }
+  console.log(formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = {
+    email: "",
+    message: "",
+  };
+  form.reset();
+})
